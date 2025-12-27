ca301c2dd83a6457a19b70ded164489294094ec1
Qasem <q.hashemi100@gmail.com>
2025-12-28 00:59:07 +0100

    Create all three routes auth, users, subscriptions

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+import  { Router } from  'express';
+
+
+const authRouter = Router();
+
+authRouter.post('/sign-up', (req, res) => res.send({title: "SIGN-UP"}));
+authRouter.post("/sign-in", (req, res) => res.send({title: "SIGN IN"}));
+authRouter.post("/sign-out", (req, res) => res.send({title: "SIGN OUT"}));
+
+export default authRouter;
