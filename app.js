6535858f938869599c9e8615f8af18c0dced64de
Qasem <q.hashemi100@gmail.com>
2025-12-30 10:00:52 +0100

    Add sign-up ,  sign-in and sign-out routes

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ import connectToDatabase from "./database/mongodb.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// important middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
+
 
 // Routers
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Subscription Tracker API!");
